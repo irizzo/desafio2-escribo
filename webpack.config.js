d438506b8712317ea89f2bab6962786c45c6f7c4
Isabelle <isabelle965rizzo@gmail.com>
2023-11-21 22:32:36 -0300

    adjusting paths

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	mode: 'production',
-	entry: './src/index.js',
+	entry: './src/api/index.js',
 	output: {
 		path: path.join(__dirname, 'dist'),
 		publicPath: '/',
