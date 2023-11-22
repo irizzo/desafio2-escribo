da56fd16852d6a307df375a120d22da7a899ef26
Isabelle <isabelle965rizzo@gmail.com>
2023-11-21 22:00:33 -0300

    re-confuguring db credentials

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,21 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./firebaseCredentials.json');
+require('dotenv/config');
+
+const serviceAccount = {
+	type: process.env.TYPE,
+	project_id: process.env.PROJECT_ID,
+	private_key_id: process.env.PRIVATE_KEY_ID,
+	private_key: process.env.PRIVATE_KEY,
+	client_email: process.env.CLIENT_EMAIL,
+	client_id: process.env.CLIENT_ID,
+	auth_uri: process.env.AUTH_URI,
+	token_uri: process.env.TOKEN_URI,
+	auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+	client_x509_cert_url: process.env.CLIENT_CERT_URL,
+	universe_domain: process.env.UNIVERSE_DOMAIN
+};
 
 initializeApp({
 	credential: cert(serviceAccount)
